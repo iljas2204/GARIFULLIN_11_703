4d9c42b229318dca4a0604894675eb6070efc69d
iljas2204 <gar_ilyas@mail.ru>
2017-10-03 17:37:06 +0300

    add 1 exercise

---
+++ LessonFor/src/HomeWork/Ex1.java
@@ -0,0 +1,22 @@
+package HomeWork;
+import java.util.Scanner;
+//Номер 10, упражнение 1
+public class Ex1 {
+
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        int n,x;
+        double s=0;
+        long z=1;
+        n=scanner.nextInt();
+        x=scanner.nextInt();
+        for (int k=0;k<=n;k++) {
+            z =z*k;
+            if (z==0) z=1;
+            if (k % 2 == 1) {
+                s = s - (Math.pow(x, 2 * k + 1) / ((2 * k + 1) * z));
+            } else s = s + (Math.pow(x, 2 * k + 1) / ((2 * k + 1) * z));
+        }
+        System.out.println(s);
+    }
+}
