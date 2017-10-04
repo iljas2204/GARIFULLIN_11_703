c152f7b4f94e4582ab4878544eb9aa80689229ab
iljas2204 <gar_ilyas@mail.ru>
2017-10-04 15:57:48 +0300

    add ex6b

--- LessonFor/src/HomeWork/Ex3.java
+++ LessonFor/src/HomeWork/Ex3.java
@@ -2,12 +2,12 @@ package HomeWork;
 
 public class Ex3 {
     public static void main(String[] args){
-        double x=1,s=1,eps=0.0001,p=-1;
+        double x=1,s=0,eps=0.0001,p=-1;
         //задача 6 б стр21.
         int k=3;
         while (Math.abs(x)>eps) {
-            x=p/(k*k);
             s=s+x;
+            x=p/(k*k);
             k=k+2;
             p=-p;
         }
