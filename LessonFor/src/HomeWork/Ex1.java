007356359389760e3a34a0d7e158701dbd7168b7
iljas2204 <gar_ilyas@mail.ru>
2017-10-07 15:42:13 +0300

    Исправил задачу

--- LessonFor/src/HomeWork/Ex1.java
+++ LessonFor/src/HomeWork/Ex1.java
@@ -6,16 +6,16 @@ public class Ex1 {
     public static void main(String[] args) {
         Scanner scanner= new Scanner(System.in);
         int n,x;
-        double s=0;
-        long z=1;
+        double p,s;
         n=scanner.nextInt();
         x=scanner.nextInt();
-        for (int k=0;k<=n;k++) {
-            z =z*k;             //быстро наступит переполнение
-            if (z==0) z=1;     //todo объяснить
+        p=x;
+        s=x;
+        for (int k=1;k<=n;k++) {
+            p=p*x*x*(2*k-1)/(k*(2*k+1));
             if (k % 2 == 1) {
-                s = s - (Math.pow(x, 2 * k + 1) / ((2 * k + 1) * z));   //плохо считаем степень
-            } else s = s + (Math.pow(x, 2 * k + 1) / ((2 * k + 1) * z));//плохо считаем степень
+                s = s - p;   //плохо считаем степень
+            } else s = s + p;//плохо считаем степень
         }
         System.out.println(s);
     }
