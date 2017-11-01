e9b909e2e2ba707fee6b15971f36263c22165aaf
iljas2204 <gar_ilyas@mail.ru>
2017-11-01 21:45:57 +0300

    добавлена семестровка

---
+++ LessonFor/src/Semestrovka/Semestr2.java
@@ -0,0 +1,25 @@
+package Semestrovka;
+
+public class Semestr2 {
+    public static void main(String[] args) {
+        double y,u0=1,u=0;
+        double [] x={-0.8,1.2,3.2};
+        int n=27;
+        for(int i=0;i<x.length;i++) {
+            if (x[i] < 0) {
+                y = Math.log(x[i] * x[i] + 2.7);
+                for (int z = 0; z < n; z++) {
+                    u = (2.0 / 3) * u0 + (y) / (3 * u0 * u0);
+                    u0=u;
+                }
+            } else {
+                y = Math.log(x[i] + 2.7);
+                for (int z = 0; z < n; z++) {
+                    u = (2.0 / 3) * u0 + (y) / (3 * u0 * u0);
+                    u0 = u;
+                }
+            }
+            System.out.println("y"+(i+1)+"=" + u);
+        }
+    }
+}
