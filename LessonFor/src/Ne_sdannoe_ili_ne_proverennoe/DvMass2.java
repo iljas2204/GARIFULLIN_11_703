f88a14fa46d1eb6f2ef6ac7dbe2227f86a85546d
iljas2204 <gar_ilyas@mail.ru>
2017-11-14 16:53:01 +0300

    Добавлены задачи на двумерные массивы

---
+++ LessonFor/src/Ne_sdannoe_ili_ne_proverennoe/DvMass2.java
@@ -0,0 +1,25 @@
+package Ne_sdannoe_ili_ne_proverennoe;
+
+import java.util.Scanner;
+//Задача 2 двумерные массивы
+public class DvMass2 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int n=scanner.nextInt();
+        int [][] a=new int[n][n];
+        for (int i = 0; i <n ; i++) {
+            for (int j = 0; j <n ; j++) {
+                a[i][j]=scanner.nextInt();
+                if(i>j){
+                    a[i][j]=0;
+                }
+            }
+        }
+        for (int i = 0; i <n ; i++) {
+            for (int j = 0; j <n ; j++) {
+                System.out.print(a[i][j]+ " ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ LessonFor/src/Ne_sdannoe_ili_ne_proverennoe/DvMass4.java
@@ -0,0 +1,56 @@
+package Ne_sdannoe_ili_ne_proverennoe;
+
+import java.util.Scanner;
+//задача 4 двумерные массивы
+public class DvMass4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x, n = scanner.nextInt(), m = scanner.nextInt();
+        if (n >= m) {
+            int[][] a = new int[n][n];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    a[i][j] = scanner.nextInt();
+                }
+            }
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (i <j) {
+                        x = a[i][j];
+                        a[i][j] = a[j][i];
+                        a[j][i] = x;
+                    }
+                }
+            }
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    System.out.print(a[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
+        if (n < m) {
+            int[][] a = new int[m][m];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    a[i][j] = scanner.nextInt();
+                }
+            }
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    if (i < j) {
+                        x = a[i][j];
+                        a[i][j] = a[j][i];
+                        a[j][i] = x;
+                    }
+                }
+            }
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    System.out.print(a[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
