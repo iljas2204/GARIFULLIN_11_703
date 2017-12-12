b65aab82447bce0ff6c67d00b3a15c00ede7cb9b
Vbayrasheva <32329206+Vbayrasheva@users.noreply.github.com>
2017-12-13 00:02:06 +0300

    Update simv5.java

--- LessonFor/src/Ne_sdannoe_ili_ne_proverennoe/simv5.java
+++ LessonFor/src/Ne_sdannoe_ili_ne_proverennoe/simv5.java
@@ -1,9 +1,9 @@
-package Ne_sdannoe_ili_ne_proverennoe;
+package Ne_sdannoe_ili_ne_proverennoe;//-
 
 public class simv5 {
     public static void main(String[] args) {
         String string1="blackblackcat";
         String string2="cat";
-        System.out.println(string1.contains(string2));
+        System.out.println(string1.contains(string2));//contradicts the condition of the task (only length() and charAt())
     }
 }
