b8dd01bc09430e3eb8557f68ffa32ab42e07885e
Vbayrasheva <32329206+Vbayrasheva@users.noreply.github.com>
2017-12-20 01:03:01 +0300

    Update simv4.java

--- LessonFor/src/Ne_sdannoe_ili_ne_proverennoe/simv4.java
+++ LessonFor/src/Ne_sdannoe_ili_ne_proverennoe/simv4.java
@@ -1,11 +1,11 @@
-package Ne_sdannoe_ili_ne_proverennoe;
+package Ne_sdannoe_ili_ne_proverennoe;//-
 
 public class simv4 {
     public static void main(String[] args) {
         String string1="sss";
         String string2="ss";
         boolean f=true;
-        for (int i = 0; i <string1.length() ; i++) {
+        for (int i = 0; i <string1.length() ; i++) {  //not taken into account the length
          if(string1.charAt(i)!=string2.charAt(i)){
              f=false;
          }
